949c8ad21a81bfc18e0fdd54fa390fcf4a0369d8
kingRywen <673895045@qq.com>
2017-12-19 13:54:46 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-// var tableSort = require('table-sort')
-import tableSort from 'table-sort';
-console.log(new tableSort.Tables('.table'))
+var tableSort = require('table-sort')
+// import tableSort from 'table-sort';
+console.log(tableSort)
